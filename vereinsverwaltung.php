7644d80348aa7d23816f9e5a657db0da0bc0c10c
hhansen06 <hhansen06@googlemail.com>
2026-02-16 23:27:54 +0100

    Add Vereinsverwaltung plugin with activation hooks

---
+++ vereinsverwaltung.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: Vereinsverwaltung
+ * Description: Verwaltung von Sparten, Ansprechpartnern und Terminen im Einstellungsbereich.
+ * Version: 1.0.0
+ * Author: Custom
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+require_once plugin_dir_path(__FILE__) . 'includes/class-vereinsverwaltung.php';
+require_once plugin_dir_path(__FILE__) . 'includes/widgets.php';
+
+function vv_vereinsverwaltung_activate(): void
+{
+    $plugin = new Vereinsverwaltung_Plugin();
+    $plugin->register_profile_rewrite();
+    flush_rewrite_rules();
+}
+
+function vv_vereinsverwaltung_deactivate(): void
+{
+    flush_rewrite_rules();
+}
+
+register_activation_hook(__FILE__, 'vv_vereinsverwaltung_activate');
+register_deactivation_hook(__FILE__, 'vv_vereinsverwaltung_deactivate');
+
+new Vereinsverwaltung_Plugin();
